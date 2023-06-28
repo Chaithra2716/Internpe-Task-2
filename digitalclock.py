6e469348425c05ba4999b10cbe3454eaee924be4
Chaithra2716 <83741811+Chaithra2716@users.noreply.github.com>
2023-06-28 22:59:14 +0530

    digitalclock.py

--- digitalclock.py
+++ digitalclock.py
@@ -1,19 +1,25 @@
-import sys
 from tkinter import *
-import time
-def timing():
-    current_time = time.strftime("%H : %M : %S")
-    clock.config(text=current_time)
-    clock.after(200,timing)
-root=Tk()
-root.geometry("600x300")
-clock=Label(root,font=("times",60,"bold"),bg="violet")
-clock.grid(row=2,column=2,pady=25,padx=100)
-timing()
-digital=Label(root,text="Digital Clock",font="times 24 bold")
-digital.grid(row=0,column=2)
- 
-nota=Label(root,text="hours        minutes        seconds",font="times 15 bold")
-nota.grid(row=3,column=2)
- 
-root.mainloop()
+from tkinter.ttk import *
+from time import strftime
+
+
+root = Tk()
+root.title('Digital Clock')
+
+
+
+def time():
+	string = strftime('%H:%M:%S %p')
+	lbl.config(text=string)
+	lbl.after(1000, time)
+
+
+lbl = Label(root, font=('Times New Roman', 40, 'bold'),
+			background='blue',
+			foreground='white')
+
+
+lbl.pack(anchor='center')
+time()
+
+mainloop()
